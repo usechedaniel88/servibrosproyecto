abc43ee1c9c61c07b791cc4a9a2cd4568291c5d3
Daniel <usechedaniel88@gmail.com>
2025-04-27 18:15:42 +0200

    link logotipo

--- src/Components/Banner/Banner.tsx
+++ src/Components/Banner/Banner.tsx
@@ -1,15 +1,18 @@
 // import React from 'react'
 import { Container } from '@mui/material'
 import BannerImage from '../../assets/Images/Banner1.png'
+import { Link } from 'react-router-dom';
 
 // type Props = {}
 
 const Banner = () => {
   return (
     <>
-    <Container sx={{backgroundColor:'#ffffff'}}>
-        <img style={{minWidth:'10%', height:'100px'}} src={BannerImage} alt='Banner' />
-    </Container>
+      <Container sx={{backgroundColor:'#ffffff'}}>
+          <Link to='/' style={{color:'#000000', textDecoration:'none'}}>
+              <img style={{minWidth:'10%', height:'100px'}} src={BannerImage} alt='Banner' />
+          </Link>
+      </Container>
     </>
   )
 }
